fef2a49daabe3bdb2999ac0d0cbade042f8f577f
TomekUrbanski <36643957+TomekUrbanski@users.noreply.github.com>
2018-05-20 09:38:15 +0200

    final

--- app/src/main/java/com/example/android/newsapp/NewsAdapter.java
+++ app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -1,12 +1,15 @@
 package com.example.android.newsapp;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -30,6 +33,12 @@ public class NewsAdapter extends ArrayAdapter<News> {
         }
 
         News currentNews = getItem(position);
+        LinearLayout listItem = listItemView.findViewById(R.id.news_item);
+        if((position & 1) == 0) {
+            listItem.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }else {
+            listItem.setBackgroundColor(Color.parseColor("#E0E0E0"));
+        }
 
         TextView newsTitle = listItemView.findViewById(R.id.titel);
         newsTitle.setText(currentNews.getmTitle());
